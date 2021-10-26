604e0e60c0b409af0d93b4e525c756b3e7de5037
mathew-fleisch <mathew.fleisch@gmail.com>
2021-10-26 06:51:43 -0700

    Try and fix bugs

--- lib/utils.bash
+++ lib/utils.bash
@@ -48,7 +48,7 @@ download_release() {
 install_version() {
   local install_type="$1"
   local version="$2"
-  local install_path="$3"
+  local install_path="$3/bin"
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-$TOOL_NAME supports release installs only"
